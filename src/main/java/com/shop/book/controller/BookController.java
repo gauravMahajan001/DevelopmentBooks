488bc4128380508ca9b2e6e2163ac5f9b61a14d8
Dell <Dell@DESKTOP-9T5M8TE>
2023-05-14 13:22:02 -0700

    Feat: Integrate validation in controller

--- src/main/java/com/shop/book/controller/BookController.java
+++ src/main/java/com/shop/book/controller/BookController.java
@@ -2,6 +2,7 @@ package com.shop.book.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.shop.book.model.BasketPrice;
 import com.shop.book.model.Book;
+import com.shop.book.validation.BookRequestValidation;
 
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
-	
+
+	@Autowired
+	private BookRequestValidation validation;
+
 	@PostMapping("/shop")
 	public BasketPrice bookShopping(@RequestBody List<Book> bookList) {
 
+		validation.bookRequestValidation(bookList);
 		return new BasketPrice();
 	}
 
--- src/test/java/com/shop/book/controller/BookControllerTest.java
+++ src/test/java/com/shop/book/controller/BookControllerTest.java
@@ -1,19 +1,37 @@
 package com.shop.book.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.shop.book.model.BasketPrice;
 import com.shop.book.model.Book;
+import com.shop.book.validation.BookRequestValidation;
 
 class BookControllerTest {
 
-	private final BookController controller = new BookController();
+	@InjectMocks
+	private BookController controller;
+	
+	@Mock
+	private BookRequestValidation validation;
+	
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+
+	}
 
 	@Test
 	@DisplayName("should return object type of BasketPrice")
@@ -25,6 +43,19 @@ class BookControllerTest {
 		BasketPrice result = controller.bookShopping(bookList);
 
 		assertThat(result).isExactlyInstanceOf(BasketPrice.class);
+	}
+	
+	@Test
+	@DisplayName("should call validation once")
+	void testBookListHasValidData() {
+		List<Book> bookList = new ArrayList<>();
+		Book book = new Book("Code", 50, 1);
+		bookList.add(book);
+		doNothing().when(validation).bookRequestValidation(bookList);
+
+		controller.bookShopping(bookList);
+
+		verify(validation, atLeastOnce()).bookRequestValidation(bookList);
 	}
 
 }
