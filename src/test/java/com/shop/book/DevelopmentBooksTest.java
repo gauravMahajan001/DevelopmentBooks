729bf9464330c901ce2da8ffdf17df88b0ac733c
Dell <Dell@DESKTOP-9T5M8TE>
2023-05-14 11:21:37 -0700

    Feat: Add basic structure

---
+++ src/test/java/com/shop/book/DevelopmentBooksTest.java
@@ -0,0 +1,13 @@
+package com.shop.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBooksTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
