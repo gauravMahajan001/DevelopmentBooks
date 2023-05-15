494cf553ae651ed3683c7f6c648cff3239394914
Dell <Dell@DESKTOP-9T5M8TE>
2023-05-15 00:47:12 -0700

    Feat: Add book combination result in model(BasketPrice)

--- src/main/java/com/shop/book/service/BookCombinaitonPrice.java
+++ src/main/java/com/shop/book/service/BookCombinaitonPrice.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import com.shop.book.model.BasketPrice;
 import com.shop.book.model.Book;
+import com.shop.book.model.BooksCombination;
 import com.shop.book.model.DiscountDto;
 
 @Component
@@ -29,8 +30,9 @@ public class BookCombinaitonPrice {
 			if (combination == bookListPerCombination.size()) {
 
 				double booksPrice = calculatePricePerBookList(bookListPerCombination);
-				DiscountDto discountDto= discount.getDiscountOnCombination(booksPrice, bookListPerCombination);
-				result.setTotalPrice(discountDto.getDiscountedBookPrice());
+				DiscountDto discountDto = discount.getDiscountOnCombination(booksPrice, bookListPerCombination);
+				setBookCombinationResult(discountDto.getDiscountedBookPrice(), discountDto.getDiscountPercantage(),
+						bookListPerCombination, result);
 			}
 		});
 		return result;
@@ -39,6 +41,17 @@ public class BookCombinaitonPrice {
 	private double calculatePricePerBookList(List<Book> bookListPerCombination) {
 
 		return bookListPerCombination.stream().map(Book::getPrice).reduce(0.0, (a, b) -> a + b);
+	}
+
+	private void setBookCombinationResult(double booksPrice, double discountPercentage, List<Book> bookList,
+			BasketPrice result) {
+
+		BooksCombination bookData = new BooksCombination(bookMetaData.getBooksName(bookList), booksPrice,
+				discountPercentage);
+
+		result.setBooksCombination(bookData);
+		result.setTotalPrice(booksPrice);
+		result.setTotalBook(bookList.size());
 	}
 
 }
