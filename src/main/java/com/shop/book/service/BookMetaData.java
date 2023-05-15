0acb9ca75da6e0458940b3073b4d0973ba516618
Dell <Dell@DESKTOP-9T5M8TE>
2023-05-14 22:41:12 -0700

    Feat: Retrieve book and reduce available count

--- src/main/java/com/shop/book/service/BookMetaData.java
+++ src/main/java/com/shop/book/service/BookMetaData.java
@@ -15,5 +15,25 @@ public class BookMetaData {
 
 		return books.stream().collect(Collectors.toMap(Book::getTitle, Book::getQuantity));
 	}
+	
+	public Book getBookAndReduceAvailableCount(int bookIndex, List<Book> booksList,
+			Map<String, Integer> bookCountMapBasedOnName) {
+
+		Book distinctBook = null;
+
+		while (distinctBook == null && bookIndex < booksList.size()) {
+
+			Book book = booksList.get(bookIndex++);
+			int bookCount = bookCountMapBasedOnName.get(book.getTitle());
+
+			if (bookCount > 0) {
+
+				bookCountMapBasedOnName.put(book.getTitle(), bookCount - 1);
+				distinctBook = book;
+			}
+
+		}
+		return distinctBook;
+	}
 
 }
--- src/test/java/com/shop/book/service/BookMetaDataTest.java
+++ src/test/java/com/shop/book/service/BookMetaDataTest.java
@@ -3,9 +3,11 @@ package com.shop.book.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,17 +17,35 @@ import com.shop.book.util.CreateBook;
 class BookMetaDataTest {
 
 	private final BookMetaData bookMetaData = new BookMetaData();
+	private List<Book> books;
+	private Map<String, Integer> bookCountMapBasedOnName;
+
+	@BeforeEach
+	public void setup() {
+		books = getBookist();
+		bookCountMapBasedOnName = getBookCountMapBasedOnName();
+	}
 
 	@Test
 	@DisplayName("should count books based on book name")
 	void testCountBooksBasedOnBookName() {
-		List<Book> books = getBookist();
 		int totalBooks = 2;
 		String bookName = "Java";
-
 		Map<String, Integer> result = bookMetaData.getBookCountMapBasedOnName(books);
 
 		assertEquals(totalBooks, result.get(bookName));
+	}
+
+	@Test
+	@DisplayName("should retrieve book and reduce available count")
+	void testBookAndReduceAvailableCount() {
+		int reduceCount = 1;
+		String bookName = "Java";
+		int bookIndex = 0;
+		Book result = bookMetaData.getBookAndReduceAvailableCount(bookIndex, books, bookCountMapBasedOnName);
+
+		assertEquals(bookName, result.getTitle());
+		assertEquals(reduceCount, bookCountMapBasedOnName.get(bookName));
 	}
 
 	private List<Book> getBookist() {
@@ -36,6 +56,16 @@ class BookMetaDataTest {
 		books.add(CreateBook.create("Test", 50, 2));
 
 		return books;
+	}
+
+	private Map<String, Integer> getBookCountMapBasedOnName() {
+		Map<String, Integer> bookCountMapBasedOnName = new HashMap<>();
+		bookCountMapBasedOnName.put("Java", 2);
+		bookCountMapBasedOnName.put("Code", 2);
+		bookCountMapBasedOnName.put("Junit", 2);
+		bookCountMapBasedOnName.put("Test", 2);
+
+		return bookCountMapBasedOnName;
 	}
 
 }
